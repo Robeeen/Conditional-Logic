65245794bb785b37fc7deb812e1ee8efe7ec21c3
Robeeen <robeeen@gmail.com>
2024-10-24 12:55:24 +0600

    adding user interface of the form

--- includes/shortcodes/shortcode.php
+++ includes/shortcodes/shortcode.php
@@ -15,6 +15,16 @@ if( !function_exists('clf_conditional_logic_form')){
             placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
+    <div class="form-group">
+        <label for="country">Select Country</label>
+            <select class="form-control" id="country" name="country">
+                <option value="">--Select Country--</option>
+                <option value="USA">USA</option>
+                <option value="Canada">Canada</option>
+                <option value="UK">UK</option>
+            </select>        
+    </div>
+
     <div class="form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
